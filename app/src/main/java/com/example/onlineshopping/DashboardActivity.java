6d180cb7a1a90c45b0725736a882e9fc14c0b9ff
DELL <srushtichaudhary333@gmail.com>
2025-02-22 18:10:54 +0530

    CHANGES

--- app/src/main/java/com/example/onlineshopping/DashboardActivity.java
+++ app/src/main/java/com/example/onlineshopping/DashboardActivity.java
@@ -34,7 +34,7 @@ public class DashboardActivity extends AppCompatActivity {
 
         sp = getSharedPreferences(ConstantSp.PREF, MODE_PRIVATE);
 
-        db = openOrCreateDatabase("AndroidASOIT.db", MODE_PRIVATE, null);
+        db = openOrCreateDatabase("AndroidOnlineShopping.db", MODE_PRIVATE, null);
         String tableQuery = "CREATE TABLE IF NOT EXISTS USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR(50), EMAIL VARCHAR(50), CONTACT BIGINT(10), PASSWORD VARCHAR(20))";
         db.execSQL(tableQuery);
 
--- app/src/main/java/com/example/onlineshopping/WishlistActivity.java
+++ app/src/main/java/com/example/onlineshopping/WishlistActivity.java
@@ -31,7 +31,7 @@ public class WishlistActivity extends AppCompatActivity {
 
         sp = getSharedPreferences(ConstantSp.PREF,MODE_PRIVATE);
 
-        db = openOrCreateDatabase("AndroidInternshipJune.db",MODE_PRIVATE,null);
+        db = openOrCreateDatabase("AndroidOnlineShopping.db",MODE_PRIVATE,null);
         String tableQuery = "CREATE TABLE IF NOT EXISTS USERS(USERID INTEGER PRIMARY KEY AUTOINCREMENT,NAME VARCHAR(50),EMAIL VARCHAR(50),CONTACT BIGINT(10),PASSWORD VARCHAR(20))";
         db.execSQL(tableQuery);
 
