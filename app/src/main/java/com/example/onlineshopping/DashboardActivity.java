68bcc3708f6699100ca1d505ddb31ac1595e42cc
DELL <srushtichaudhary333@gmail.com>
2025-03-22 16:52:09 +0530

    changes code

--- app/src/main/java/com/example/onlineshopping/DashboardActivity.java
+++ app/src/main/java/com/example/onlineshopping/DashboardActivity.java
@@ -63,25 +63,7 @@ public class DashboardActivity extends AppCompatActivity {
         profile = findViewById(R.id.dashboard_profile);
         delete = findViewById(R.id.dashboard_delete_profile);
         logout = findViewById(R.id.dashboard_logout);
-
-        navigationDemo = findViewById(R.id.dashboard_navigation);
-        navigationDemo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(DashboardActivity.this, NavDemoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        bottomNav = findViewById(R.id.dashboard_bottom);
-        bottomNav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(DashboardActivity.this, BottomNavDemoActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        
         tabDemo = findViewById(R.id.dashboard_tab_demo);
         tabDemo.setOnClickListener(new View.OnClickListener() {
             @Override
